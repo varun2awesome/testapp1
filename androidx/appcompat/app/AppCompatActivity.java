bf18cfa315152d63cf0932f1dd7c685e0ab186f1
Varun Verma <64241983+varun2awesome@users.noreply.github.com>
2023-02-13 13:59:16 +0530

    Add files via upload

---
+++ androidx/appcompat/app/AppCompatActivity.java
@@ -0,0 +1,353 @@
+package androidx.appcompat.app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import androidx.annotation.CallSuper;
+import androidx.annotation.ContentView;
+import androidx.annotation.IdRes;
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.StyleRes;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.view.ActionMode;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.VectorEnabledTintResources;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NavUtils;
+import androidx.core.app.TaskStackBuilder;
+import androidx.fragment.app.FragmentActivity;
+/* loaded from: classes.dex */
+public class AppCompatActivity extends FragmentActivity implements AppCompatCallback, TaskStackBuilder.SupportParentable, ActionBarDrawerToggle.DelegateProvider {
+    private AppCompatDelegate mDelegate;
+    private Resources mResources;
+
+    /* JADX INFO: Access modifiers changed from: protected */
+    public void onNightModeChanged(int i) {
+    }
+
+    public void onPrepareSupportNavigateUpTaskStack(@NonNull TaskStackBuilder taskStackBuilder) {
+    }
+
+    @Override // androidx.appcompat.app.AppCompatCallback
+    @CallSuper
+    public void onSupportActionModeFinished(@NonNull ActionMode actionMode) {
+    }
+
+    @Override // androidx.appcompat.app.AppCompatCallback
+    @CallSuper
+    public void onSupportActionModeStarted(@NonNull ActionMode actionMode) {
+    }
+
+    @Deprecated
+    public void onSupportContentChanged() {
+    }
+
+    @Override // androidx.appcompat.app.AppCompatCallback
+    @Nullable
+    public ActionMode onWindowStartingSupportActionMode(@NonNull ActionMode.Callback callback) {
+        return null;
+    }
+
+    @Deprecated
+    public void setSupportProgress(int i) {
+    }
+
+    @Deprecated
+    public void setSupportProgressBarIndeterminate(boolean z) {
+    }
+
+    @Deprecated
+    public void setSupportProgressBarIndeterminateVisibility(boolean z) {
+    }
+
+    @Deprecated
+    public void setSupportProgressBarVisibility(boolean z) {
+    }
+
+    public AppCompatActivity() {
+    }
+
+    @ContentView
+    public AppCompatActivity(@LayoutRes int i) {
+        super(i);
+    }
+
+    @Override // android.app.Activity, android.view.ContextThemeWrapper, android.content.ContextWrapper
+    protected void attachBaseContext(Context context) {
+        super.attachBaseContext(getDelegate().attachBaseContext2(context));
+    }
+
+    /* JADX INFO: Access modifiers changed from: protected */
+    @Override // androidx.fragment.app.FragmentActivity, androidx.activity.ComponentActivity, androidx.core.app.ComponentActivity, android.app.Activity
+    public void onCreate(@Nullable Bundle bundle) {
+        AppCompatDelegate delegate = getDelegate();
+        delegate.installViewFactory();
+        delegate.onCreate(bundle);
+        super.onCreate(bundle);
+    }
+
+    @Override // android.app.Activity, android.view.ContextThemeWrapper, android.content.ContextWrapper, android.content.Context
+    public void setTheme(@StyleRes int i) {
+        super.setTheme(i);
+        getDelegate().setTheme(i);
+    }
+
+    @Override // android.app.Activity
+    protected void onPostCreate(@Nullable Bundle bundle) {
+        super.onPostCreate(bundle);
+        getDelegate().onPostCreate(bundle);
+    }
+
+    @Nullable
+    public ActionBar getSupportActionBar() {
+        return getDelegate().getSupportActionBar();
+    }
+
+    public void setSupportActionBar(@Nullable Toolbar toolbar) {
+        getDelegate().setSupportActionBar(toolbar);
+    }
+
+    @Override // android.app.Activity
+    @NonNull
+    public MenuInflater getMenuInflater() {
+        return getDelegate().getMenuInflater();
+    }
+
+    @Override // androidx.activity.ComponentActivity, android.app.Activity
+    public void setContentView(@LayoutRes int i) {
+        getDelegate().setContentView(i);
+    }
+
+    @Override // androidx.activity.ComponentActivity, android.app.Activity
+    public void setContentView(View view) {
+        getDelegate().setContentView(view);
+    }
+
+    @Override // androidx.activity.ComponentActivity, android.app.Activity
+    public void setContentView(View view, ViewGroup.LayoutParams layoutParams) {
+        getDelegate().setContentView(view, layoutParams);
+    }
+
+    @Override // androidx.activity.ComponentActivity, android.app.Activity
+    public void addContentView(View view, ViewGroup.LayoutParams layoutParams) {
+        getDelegate().addContentView(view, layoutParams);
+    }
+
+    @Override // androidx.activity.ComponentActivity, android.app.Activity, android.content.ComponentCallbacks
+    public void onConfigurationChanged(@NonNull Configuration configuration) {
+        super.onConfigurationChanged(configuration);
+        if (this.mResources != null) {
+            this.mResources.updateConfiguration(configuration, super.getResources().getDisplayMetrics());
+        }
+        getDelegate().onConfigurationChanged(configuration);
+    }
+
+    /* JADX INFO: Access modifiers changed from: protected */
+    @Override // androidx.fragment.app.FragmentActivity, android.app.Activity
+    public void onPostResume() {
+        super.onPostResume();
+        getDelegate().onPostResume();
+    }
+
+    /* JADX INFO: Access modifiers changed from: protected */
+    @Override // androidx.fragment.app.FragmentActivity, android.app.Activity
+    public void onStart() {
+        super.onStart();
+        getDelegate().onStart();
+    }
+
+    /* JADX INFO: Access modifiers changed from: protected */
+    @Override // androidx.fragment.app.FragmentActivity, android.app.Activity
+    public void onStop() {
+        super.onStop();
+        getDelegate().onStop();
+    }
+
+    @Override // android.app.Activity
+    public <T extends View> T findViewById(@IdRes int i) {
+        return (T) getDelegate().findViewById(i);
+    }
+
+    @Override // androidx.fragment.app.FragmentActivity, androidx.activity.ComponentActivity, android.app.Activity, android.view.Window.Callback
+    public final boolean onMenuItemSelected(int i, @NonNull MenuItem menuItem) {
+        if (super.onMenuItemSelected(i, menuItem)) {
+            return true;
+        }
+        ActionBar supportActionBar = getSupportActionBar();
+        if (menuItem.getItemId() != 16908332 || supportActionBar == null || (supportActionBar.getDisplayOptions() & 4) == 0) {
+            return false;
+        }
+        return onSupportNavigateUp();
+    }
+
+    /* JADX INFO: Access modifiers changed from: protected */
+    @Override // androidx.fragment.app.FragmentActivity, android.app.Activity
+    public void onDestroy() {
+        super.onDestroy();
+        getDelegate().onDestroy();
+    }
+
+    @Override // android.app.Activity
+    protected void onTitleChanged(CharSequence charSequence, int i) {
+        super.onTitleChanged(charSequence, i);
+        getDelegate().setTitle(charSequence);
+    }
+
+    public boolean supportRequestWindowFeature(int i) {
+        return getDelegate().requestWindowFeature(i);
+    }
+
+    @Override // androidx.fragment.app.FragmentActivity
+    public void supportInvalidateOptionsMenu() {
+        getDelegate().invalidateOptionsMenu();
+    }
+
+    @Override // android.app.Activity
+    public void invalidateOptionsMenu() {
+        getDelegate().invalidateOptionsMenu();
+    }
+
+    @Nullable
+    public ActionMode startSupportActionMode(@NonNull ActionMode.Callback callback) {
+        return getDelegate().startSupportActionMode(callback);
+    }
+
+    public void onCreateSupportNavigateUpTaskStack(@NonNull TaskStackBuilder taskStackBuilder) {
+        taskStackBuilder.addParentStack(this);
+    }
+
+    public boolean onSupportNavigateUp() {
+        Intent supportParentActivityIntent = getSupportParentActivityIntent();
+        if (supportParentActivityIntent != null) {
+            if (supportShouldUpRecreateTask(supportParentActivityIntent)) {
+                TaskStackBuilder create = TaskStackBuilder.create(this);
+                onCreateSupportNavigateUpTaskStack(create);
+                onPrepareSupportNavigateUpTaskStack(create);
+                create.startActivities();
+                try {
+                    ActivityCompat.finishAffinity(this);
+                    return true;
+                } catch (IllegalStateException unused) {
+                    finish();
+                    return true;
+                }
+            }
+            supportNavigateUpTo(supportParentActivityIntent);
+            return true;
+        }
+        return false;
+    }
+
+    @Override // androidx.core.app.TaskStackBuilder.SupportParentable
+    @Nullable
+    public Intent getSupportParentActivityIntent() {
+        return NavUtils.getParentActivityIntent(this);
+    }
+
+    public boolean supportShouldUpRecreateTask(@NonNull Intent intent) {
+        return NavUtils.shouldUpRecreateTask(this, intent);
+    }
+
+    public void supportNavigateUpTo(@NonNull Intent intent) {
+        NavUtils.navigateUpTo(this, intent);
+    }
+
+    @Override // android.app.Activity, android.view.Window.Callback
+    public void onContentChanged() {
+        onSupportContentChanged();
+    }
+
+    @Override // androidx.appcompat.app.ActionBarDrawerToggle.DelegateProvider
+    @Nullable
+    public ActionBarDrawerToggle.Delegate getDrawerToggleDelegate() {
+        return getDelegate().getDrawerToggleDelegate();
+    }
+
+    @Override // android.app.Activity, android.view.Window.Callback
+    public boolean onMenuOpened(int i, Menu menu) {
+        return super.onMenuOpened(i, menu);
+    }
+
+    @Override // androidx.activity.ComponentActivity, android.app.Activity, android.view.Window.Callback
+    public void onPanelClosed(int i, @NonNull Menu menu) {
+        super.onPanelClosed(i, menu);
+    }
+
+    @Override // androidx.activity.ComponentActivity, androidx.core.app.ComponentActivity, android.app.Activity
+    protected void onSaveInstanceState(@NonNull Bundle bundle) {
+        super.onSaveInstanceState(bundle);
+        getDelegate().onSaveInstanceState(bundle);
+    }
+
+    @NonNull
+    public AppCompatDelegate getDelegate() {
+        if (this.mDelegate == null) {
+            this.mDelegate = AppCompatDelegate.create(this, this);
+        }
+        return this.mDelegate;
+    }
+
+    @Override // androidx.core.app.ComponentActivity, android.app.Activity, android.view.Window.Callback
+    public boolean dispatchKeyEvent(KeyEvent keyEvent) {
+        int keyCode = keyEvent.getKeyCode();
+        ActionBar supportActionBar = getSupportActionBar();
+        if (keyCode == 82 && supportActionBar != null && supportActionBar.onMenuKeyEvent(keyEvent)) {
+            return true;
+        }
+        return super.dispatchKeyEvent(keyEvent);
+    }
+
+    @Override // android.view.ContextThemeWrapper, android.content.ContextWrapper, android.content.Context
+    public Resources getResources() {
+        if (this.mResources == null && VectorEnabledTintResources.shouldBeUsed()) {
+            this.mResources = new VectorEnabledTintResources(this, super.getResources());
+        }
+        Resources resources = this.mResources;
+        return resources == null ? super.getResources() : resources;
+    }
+
+    private boolean performMenuItemShortcut(KeyEvent keyEvent) {
+        Window window;
+        return (Build.VERSION.SDK_INT >= 26 || keyEvent.isCtrlPressed() || KeyEvent.metaStateHasNoModifiers(keyEvent.getMetaState()) || keyEvent.getRepeatCount() != 0 || KeyEvent.isModifierKey(keyEvent.getKeyCode()) || (window = getWindow()) == null || window.getDecorView() == null || !window.getDecorView().dispatchKeyShortcutEvent(keyEvent)) ? false : true;
+    }
+
+    @Override // android.app.Activity, android.view.KeyEvent.Callback
+    public boolean onKeyDown(int i, KeyEvent keyEvent) {
+        if (performMenuItemShortcut(keyEvent)) {
+            return true;
+        }
+        return super.onKeyDown(i, keyEvent);
+    }
+
+    @Override // android.app.Activity
+    public void openOptionsMenu() {
+        ActionBar supportActionBar = getSupportActionBar();
+        if (getWindow().hasFeature(0)) {
+            if (supportActionBar == null || !supportActionBar.openOptionsMenu()) {
+                super.openOptionsMenu();
+            }
+        }
+    }
+
+    @Override // android.app.Activity
+    public void closeOptionsMenu() {
+        ActionBar supportActionBar = getSupportActionBar();
+        if (getWindow().hasFeature(0)) {
+            if (supportActionBar == null || !supportActionBar.closeOptionsMenu()) {
+                super.closeOptionsMenu();
+            }
+        }
+    }
+}
